88ffe85f401ab3e15c9ed3e04d0a52181e002ba0
Andre Colomb <andre.colomb@ift.uni-stuttgart.de>
2012-11-12 20:04:56 +0100

    Derive RemoteCommandResponse from ATCommandResponse. Remove duplicate members and CommandStatusType enum. Use common ParseValue() method.

--- XBee/Frames/RemoteCommandResponse.cs
+++ XBee/Frames/RemoteCommandResponse.cs
@@ -5,27 +5,13 @@ using XBee.Utils;
 
 namespace XBee.Frames
 {
-    public class RemoteCommandResponse : XBeeFrame
+    public class RemoteCommandResponse : ATCommandResponse
     {
-        public enum CommandStatusType
-        {
-            Ok = 0x00,
-            Error = 0x01,
-            InvalidCommand = 0x02,
-            InvalidParameter = 0x03,
-            TransmissionFailed = 0x04
-        }
-
-        private readonly PacketParser parser;
-
         public XBeeNode Source { get; private set; }
-        public AT Command { get; private set; }
-        public CommandStatusType CommandStatus { get; private set; }
-        public ATValue Value { get; private set; }
 
-        public RemoteCommandResponse(PacketParser parser)
+        public RemoteCommandResponse(PacketParser parser) :
+            base(parser)
         {
-            this.parser = parser;
             CommandId = XBeeAPICommandId.REMOTE_AT_COMMAND_RESPONSE;
         }
 
@@ -38,36 +24,10 @@ namespace XBee.Frames
         {
             FrameId = (byte) parser.ReadByte();
             Source = new XBeeNode { Address64 = parser.ReadAddress64(), Address16 = parser.ReadAddress16() };
-
             Command = parser.ReadATCommand();
             CommandStatus = (CommandStatusType) parser.ReadByte();
 
-            if (Command == AT.NodeDiscover)
-                ParseNetworkDiscovery();
-
-            var type = ((ATAttribute)Command.GetAttr()).ValueType;
-
-            if ((type != ATValueType.None) && parser.HasMoreData()) {
-                switch (type) {
-                    case ATValueType.Number:
-                        var vData = parser.ReadData();
-                        Value = new ATLongValue().FromByteArray(vData);
-                        break;
-                    case ATValueType.HexString:
-                        var hexData = parser.ReadData();
-                        Value = new ATStringValue(ByteUtils.ToBase16(hexData));
-                        break;
-                    case ATValueType.String:
-                        var str = parser.ReadData();
-                        Value = new ATStringValue(Encoding.UTF8.GetString(str));
-                        break;
-                }
-            }
-        }
-
-        private void ParseNetworkDiscovery()
-        {
-            throw new NotImplementedException();
+            ParseValue();
         }
     }
 }
