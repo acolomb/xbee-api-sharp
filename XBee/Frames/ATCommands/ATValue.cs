fc87731c367a828239e2eb4d5556d6f416481257
Andre Colomb <andre.colomb@ift.uni-stuttgart.de>
2012-11-13 09:24:10 +0100

    Add new ATValue-derived classes ATNodeDiscoverValue and ATPanDescriptorValue. These allow parsing the response of respective AT commands.

--- XBee/Frames/ATCommands/ATValue.cs
+++ XBee/Frames/ATCommands/ATValue.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Text;
 
 namespace XBee.Frames.ATCommands
@@ -10,6 +11,8 @@ namespace XBee.Frames.ATCommands
         String,
         HexString,
         NodeIdentifier,
+        PanDescriptor,
+        NodeDiscover,
     }
 
 
@@ -140,6 +143,131 @@ namespace XBee.Frames.ATCommands
         public override byte[] ToByteArray()
         {
             return Encoding.ASCII.GetBytes(Value);
+        }
+    }
+
+
+
+    public class ATNodeDiscoverValue : ATValue
+    {
+        private const int minLength = 2+4+4+1+1;
+
+        public XBeeNode Source { get; set; }
+        public ATLongValue SignalStrength { get; set; }
+        public ATNodeIdentifierValue NodeIdentifier { get; set; }
+
+        public ATNodeDiscoverValue()
+        { }
+
+        public override ATValue FromByteArray(byte[] value)
+        {
+            if (value.Length == 1 && value[0] == 0)	//end of records
+                return new ATNodeDiscoverValue();
+
+            if (value.Length < minLength)
+                throw new InvalidCastException("Node Discover Response has too few bytes.");
+
+            var parser = new PacketParser(value);
+
+            Source = new XBeeNode { Address16 = parser.ReadAddress16(), Address64 = parser.ReadAddress64() };
+            SignalStrength = new ATLongValue((ulong) parser.ReadByte());
+            NodeIdentifier = new ATNodeIdentifierValue(parser.ReadString());
+            return this;
+        }
+
+        public override byte[] ToByteArray()
+        {
+            var stream = new MemoryStream(minLength + NodeIdentifier.Value.Length);
+
+            byte[] buffer;
+            buffer = Source.Address16.GetAddress();
+            stream.Write(buffer, 0, buffer.Length);
+
+            stream.WriteByte(SignalStrength.ToByteArray()[0]);
+
+            buffer = NodeIdentifier.ToByteArray();
+            stream.Write(buffer, 0, buffer.Length);
+            stream.WriteByte(0);
+
+            return stream.ToArray();
+        }
+    }
+
+
+
+    public class ATPanDescriptorValue : ATValue
+    {
+        private const int packetLength = 8+2+1+1+1+1+1+2+1+1+3;
+
+        public enum CoordAddrMode {
+            ShortAddress16Bit = 0x02,
+            LongAddress64Bit = 0x03,
+        };
+
+
+        public XBeeAddress64 Coordinator { get; set; }
+        public ATLongValue PanId { get; set; }
+        public CoordAddrMode AddressMode { get; set; }
+        public ATLongValue Channel { get; set; }
+        public ATLongValue SecurityUse { get; set; }
+        public ATLongValue ACLEntry { get; set; }
+        public ATLongValue SecurityFailure { get; set; }
+        public ATLongValue SuperFrameSpec { get; set; }
+        public ATLongValue GtsPermit { get; set; }
+        public ATLongValue SignalStrength { get; set; }
+        public byte[] TimeStamp { get; set; }
+
+        public ATPanDescriptorValue()
+        { }
+
+        public override ATValue FromByteArray(byte[] value)
+        {
+            if (value.Length < packetLength)
+                throw new InvalidCastException("Pan Descriptor has too few bytes.");
+
+            var parser = new PacketParser(value);
+
+            Coordinator = parser.ReadAddress64();
+            PanId = new ATLongValue(parser.ReadUInt16());
+            AddressMode = (CoordAddrMode) parser.ReadByte();
+            Channel = new ATLongValue((ulong) parser.ReadByte());
+            SecurityUse = new ATLongValue((ulong) parser.ReadByte());
+            ACLEntry = new ATLongValue((ulong) parser.ReadByte());
+            SecurityFailure = new ATLongValue((ulong) parser.ReadByte());
+            SuperFrameSpec = new ATLongValue(parser.ReadUInt16());
+            GtsPermit = new ATLongValue((ulong) parser.ReadByte());
+            SignalStrength = new ATLongValue((ulong) parser.ReadByte());
+            TimeStamp = parser.ReadData();
+
+            return this;
+        }
+
+        public override byte[] ToByteArray()
+        {
+            var stream = new MemoryStream(packetLength);
+
+            byte[] buffer;
+            buffer = Coordinator.GetAddress();
+            stream.Write(buffer, 0, buffer.Length);
+
+            buffer = PanId.ToByteArray();
+            stream.Write(buffer, 0, buffer.Length);
+
+            stream.WriteByte((byte) AddressMode);
+            stream.WriteByte(Channel.ToByteArray()[0]);
+            stream.WriteByte(SecurityUse.ToByteArray()[0]);
+            stream.WriteByte(ACLEntry.ToByteArray()[0]);
+            stream.WriteByte(SecurityFailure.ToByteArray()[0]);
+
+            buffer = SuperFrameSpec.ToByteArray();
+            stream.Write(buffer, 0, buffer.Length);
+
+            stream.WriteByte(GtsPermit.ToByteArray()[0]);
+            stream.WriteByte(SignalStrength.ToByteArray()[0]);
+
+            stream.Write(TimeStamp, 0, TimeStamp.Length);
+
+            return stream.ToArray();
         }
     }
 }
