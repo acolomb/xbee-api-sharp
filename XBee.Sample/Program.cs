2f854da6d3c73ad666ea5da61890a030148acee9
Andre Colomb <andre.colomb@ift.uni-stuttgart.de>
2012-11-21 14:33:30 +0100

    Integrate asynchronous response handling via XBeeResponseTracker into sample program.

--- XBee.Sample/Program.cs
+++ XBee.Sample/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using XBee;
 using XBee.Frames;
 using XBee.Frames.ATCommands;
 using XBee.Utils;
@@ -15,6 +16,10 @@ namespace XBee.Sample
                 ApiVersion = ApiVersion.S1
             };
             bee.SetConnection(new SerialConnection("COM10", 115200));
+
+            var tracker = new XBeeResponseTracker();
+            bee.FrameReceived += tracker.HandleFrameReceived;
+            tracker.RegisterDefaultFrameHandler(HandleAnyFrame);
 
             XBeeFrame frame;
 
@@ -45,7 +50,7 @@ namespace XBee.Sample
                 var value = (ATLongValue) atResponse.Value;
                 Console.WriteLine("{0} status: {1}, result: {2}",
                                   atResponse.Command, atResponse.CommandStatus,
-                                  value == null ? "<none>" : value.Value.ToString());
+                                  value == null ? "<none>" : value.Value.ToString("X4"));
             }
 
             request = new ATCommand(AT.FirmwareVersion) { FrameId = 1 };
@@ -63,18 +68,49 @@ namespace XBee.Sample
             frame = bee.ExecuteQuery(request, 1000);
             if (frame != null) {
                 var atResponse = (ATCommandResponse) frame;
-                Console.WriteLine(String.Format("{0} status: {1}", atResponse.Command, atResponse.CommandStatus));
-                value = atResponse.Value;
-                if (value != null) Console.WriteLine(String.Format("Destination Node: {0}", ((ATNodeIdentifierValue) value).Value));
+                var value = (ATNodeIdentifierValue) atResponse.Value;
+                Console.WriteLine("{0} status: {1}, result: {2}",
+                                  atResponse.Command, atResponse.CommandStatus,
+                                  value == null ? "<none>" : value.Value);
             }
 
-            request = new ATCommand(AT.ActiveScan) { FrameId = 20 };
-            //request = new ATCommand(AT.NodeDiscover) { FrameId = 1 };
+            request = new ATCommand(AT.ActiveScan) { FrameId = tracker.RegisterResponseHandler(HandleActiveScan) };
+            bee.Execute(request);
+            request = new ATCommand(AT.NodeDiscover) { FrameId = tracker.RegisterResponseHandler(HandleNodeDiscover) };
             bee.Execute(request);
 
             do {
                 Thread.Sleep(10000);
             } while (false);
+        }
+
+        public static void HandleAnyFrame(object sender, FrameReceivedEventArgs args)
+        {
+            Console.WriteLine("Received {0} frame.", args.Response.CommandId);
+        }
+
+        public static void HandleActiveScan(object sender, FrameReceivedEventArgs args)
+        {
+            var atResponse = (ATCommandResponse) args.Response;
+            var pan = (ATPanDescriptorValue) atResponse.Value;
+            if (pan == null) {
+                (sender as XBeeResponseTracker).UnregisterResponseHandler(atResponse.FrameId);
+                Console.WriteLine("{0} status: {1}, end of records", atResponse.Command, atResponse.CommandStatus);
+            } else {
+                Console.WriteLine("{0} status: {1}, PAN ID {2}", atResponse.Command, atResponse.CommandStatus, pan.PanId);
+            }
+        }
+
+        public static void HandleNodeDiscover(object sender, FrameReceivedEventArgs args)
+        {
+            var atResponse = (ATCommandResponse) args.Response;
+            var node = (ATNodeDiscoverValue) atResponse.Value;
+            if (node == null) {
+                (sender as XBeeResponseTracker).UnregisterResponseHandler(atResponse.FrameId);
+                Console.WriteLine("{0} status: {1}, end of records", atResponse.Command, atResponse.CommandStatus);
+            } else {
+                Console.WriteLine("{0} status: {1}, node {2}", atResponse.Command, atResponse.CommandStatus, node.NodeIdentifier.Value);
+            }
         }
     }
 }
