a836a5efbad53d2500cd7e2b4b5df0f09ae2c899
Andre Colomb <andre.colomb@ift.uni-stuttgart.de>
2012-11-26 18:38:42 +0100

    Provide IFormattable interface in XBeeAddress by concatenating each byte according to the given format specifier.

--- XBee/XBeeAddress.cs
+++ XBee/XBeeAddress.cs
@@ -1,14 +1,25 @@
-﻿using XBee.Utils;
+﻿using System;
+using System.Text;
+using XBee.Utils;
 
 namespace XBee
 {
-    public abstract class XBeeAddress
+    public abstract class XBeeAddress : IFormattable
     {
         public abstract byte[] GetAddress();
 
         public override string ToString()
         {
             return ByteUtils.ToBase16(GetAddress());
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider = null)
+        {
+            var sb = new StringBuilder();
+            foreach (var b in GetAddress()) {
+                sb.Append(b.ToString(format, formatProvider));
+            }
+            return sb.ToString();
         }
     }
 }
